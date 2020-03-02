ac361b2a3aca1dfd8979436fbbaec2e819ffd024
henriquefrinhani <henriquefrinhani@hotmail.com>
2020-03-02 20:29:24 -0300

    Estado e Cidade

---
+++ src/main/java/com/nelioalves/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,14 @@
+package com.nelioalves.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.domain.Cidade;
+import com.nelioalves.cursomc.domain.Estado;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+	
+}

e08b8add65301dc726cb84cb0ac7b1eba80f5b7c
Henrique Frinhani <henrique.oliveira@objective.com.br>
2020-06-24 21:43:39 -0300

    Endpoint /pedidos/{id} /disponibilizado

---
+++ src/main/java/com/nelioalves/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.nelioalves.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.cursomc.domain.Pedido;
+import com.nelioalves.cursomc.repositories.PedidoRepository;
+import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundExceptions;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() ->  new ObjectNotFoundExceptions(
+				"Objeto não encontrado! id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+
+}
